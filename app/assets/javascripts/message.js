69c25cff671fc3e3635123bda82d7925be4683cd
Wa-www <0nicoco0@gmail.com>
2019-10-04 00:43:46 +0900

    ajax_2
    とりあえずデータのやり取りはできるようになった

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,22 +1,56 @@
 $(function(){
   // console.log('ok');
+var html_source=$(".detail");
+
+  function addHtml(aaa){
+    var html=`
+    <div class="detail">
+    <div class="detail__user">
+      ${aaa.user_name}
+    </div>
+    <div class="detail__date">
+      ${aaa.created_at}
+    </div>
+    <div class="detail__contents">
+        ${aaa.content}
+        ${aaa.image}
+    </div>
+  </div>
+  
+  `
+  html_source.append(html)
+
+  }
+
+
   $('#message_id').on('submit',function(e){
     e.preventDefault();
     // console.log(this);
-    var formData = new FormData();
-    var text=this.text;
-    var file=this.file;
-    formData.append('text',text);
-    formData.append('file',file); //左と右が何？
-    console.log(formData);
+
+    var dt=new FormData($(this).get(0));
+    dt.append(this, $('input[type=file]')[0].files[0]); //いらない？
+
+    // var dt = new FormData();
+    // var text=$(this).text;
+    // var file=$(this).file;
+    // dt.append('text',text);
+    // dt.append('file',file); //左と右が何？
+    // console.log(dt);
+
+
     var url = $(this).attr('action')
+    console.log(url);
     $.ajax({
       url:url,
       type:"POST",
-      data:formData,
+      data:dt,
       dataType: 'json',
       processData: false,
       contentType: false,
+    })
+    .done(function(data){
+      console.log(data);
+      var html=addHtml(data);
     })
   })
 
