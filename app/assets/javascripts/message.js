a86bcdbca32e2522988260655b16a28d74d77f3f
Wa-www <0nicoco0@gmail.com>
2019-10-05 20:48:44 +0900

    message_ajax4
    あとはturbolinkのみ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,71 @@
 $(function(){
+// document.addEventListener ('turbolinks:load', function() {
+
   // console.log('ok');
 var html_source=$(".detail");
 
+
   function addHtml(aaa){
+    console.log(aaa.content);
+    console.log(aaa.image);
+
+    if (aaa.content != null && aaa.image != null) {
     var html=`
-    <div class="detail">
+    <div class="details">
     <div class="detail__user">
       ${aaa.user_name}
     </div>
     <div class="detail__date">
-      ${aaa.created_at}
+      <time>${aaa.created_at}</time>
     </div>
     <div class="detail__contents">
         ${aaa.content}
-        ${aaa.image}
+        <img src="${aaa.image}" width="128" height="128">
     </div>
-  </div>
-  
-  `
-  html_source.append(html)
+    </div>`
+    html_source.append(html)  
+    console.log(html);
 
+    }else if (aaa.content != "" && aaa.image == null){
+      var html=`
+      <div class="details">
+      <div class="detail__user">
+        ${aaa.user_name}
+      </div>
+      <div class="detail__date">
+        <time>${aaa.created_at}</time>
+      </div>
+      <div class="detail__contents">
+          ${aaa.content}
+      </div>
+      </div>`
+      html_source.append(html)  
+      console.log(html)
+
+    }else if (aaa.content == "" && aaa.image != null){
+      var html=`
+      <div class="details">
+      <div class="detail__user">
+        ${aaa.user_name}
+      </div>
+      <div class="detail__date">
+        <time>${aaa.created_at}</time>
+      </div>
+      <div class="detail__contents">
+      <img src="${aaa.image}" width="128" height="128">
+      </div>
+      </div>
+      `
+      html_source.append(html)  
+    }
   }
 
-
+  // $(document).ready(function(){
   $('#message_id').on('submit',function(e){
     e.preventDefault();
     // console.log(this);
 
     var dt=new FormData($(this).get(0));
-    dt.append(this, $('input[type=file]')[0].files[0]); //いらない？
-
-    // var dt = new FormData();
-    // var text=$(this).text;
-    // var file=$(this).file;
-    // dt.append('text',text);
-    // dt.append('file',file); //左と右が何？
-    // console.log(dt);
-
 
     var url = $(this).attr('action')
     console.log(url);
@@ -50,8 +79,17 @@ var html_source=$(".detail");
     })
     .done(function(data){
       console.log(data);
-      var html=addHtml(data);
+      var createhtml=addHtml(data);
+      $('.message-input').val('');
+      // $("details").animate({scrollTop:$('details').offset().top});
+      $('.detail').animate({ scrollTop: $('.detail')[0].scrollHeight });
+      $('.send').attr('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+
     })
   })
-
-})
+  // })
+// })
