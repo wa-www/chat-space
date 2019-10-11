95c43300749033dfa08e0fb6224f2dd7f6baa6ad
Wa-www <0nicoco0@gmail.com>
2019-10-11 20:59:21 +0900

    auto_reload_finished
    自動更新機能完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,66 +49,35 @@ var html_source=$(".detail");
     })
 
     })
-  })
 
 
-$(function() {
-//省略
   if(location.href.match(/\/groups\/\d+\/messages/)){
   setInterval(reloadMessages, 5000);
   }
 
   function reloadMessages() {
 
-    // if($('.detail')[0]) {
-
     var groupId = $('.details').data('group-id');
-    // var a='/groups/'+group_id+'/api/messages';
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.details:last').data('message-id');
-    console.log(groupId);
-    console.log(last_message_id);
-
-  // } else {
-  //   var last_message_id = 0;
-  // }
 
     url = '/groups/' + groupId + '/api/messages';
 
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: url,
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id,group_id:groupId}
     })
     .done(function(messages) {
-      console.log(messages);
-      //追加するHTMLの入れ物を作る
-      var insertHTML = '';
 
-    $(document).on('turbolinks:load', function() {
-      if (messages !="null") {
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         messages.forEach(function(message){
-          // insertHTML=addHtml(message);
-          addHtml(message);  //これでaddHtmlのなかで追記される
+          addHtml(message); 
           $('.detail').animate({scrollTop: $('.detail')[0].scrollHeight});
           console.log(message);
         })
-      }else{
-      }
-  })
-
-      //メッセージが入ったHTMLを取得
-
-      //メッセージを追加
-
     })
     .fail(function() {
       console.log('エラー');
     });
   }; 
-});
+})
