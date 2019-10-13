ef739f1f230d453044a27a65177e0b125686f196
Wa-www <0nicoco0@gmail.com>
2019-10-13 17:58:50 +0900

    modify3
    修正３

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, '自身のアプリケーション名'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:wa-www/chat-space.git'
